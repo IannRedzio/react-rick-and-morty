e816e9c958c8fa33e89247fe039e5355311b6dd7
IannRedzio <ianredzio13@gmail.com>
2022-06-27 00:36:13 -0300

    fix button pages

--- src/components/NavPage.js
+++ src/components/NavPage.js
@@ -3,12 +3,13 @@ import { Container, Button } from "@chakra-ui/react";
 
 const NavPage = ({ page, setPage }) => {
   return (
-    <Container display="flex" justifyContent="space-between">
+    <Container display="flex" justifyContent="center">
       <Button
         colorScheme="teal"
         bg="#00b1c9"
         color="#80fc63"
         onClick={() => setPage(page - 1)}
+        mr={10}
       >
         Prev Page
       </Button>
