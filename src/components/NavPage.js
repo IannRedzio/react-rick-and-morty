bdc3f266486c84c48af82d262f61087c49851186
IannRedzio <ianredzio13@gmail.com>
2022-06-27 00:12:47 -0300

    add buttons for landing and navbar

--- src/components/NavPage.js
+++ src/components/NavPage.js
@@ -4,8 +4,22 @@ import { Container, Button } from "@chakra-ui/react";
 const NavPage = ({ page, setPage }) => {
   return (
     <Container display="flex" justifyContent="space-between">
-      <Button onClick={() => setPage(page - 1)}>Prev Page</Button>
-      <Button onClick={() => setPage(page + 1)}>Next Page</Button>
+      <Button
+        colorScheme="teal"
+        bg="#00b1c9"
+        color="#80fc63"
+        onClick={() => setPage(page - 1)}
+      >
+        Prev Page
+      </Button>
+      <Button
+        colorScheme="teal"
+        bg="#00b1c9"
+        color="#80fc63"
+        onClick={() => setPage(page + 1)}
+      >
+        Next Page
+      </Button>
     </Container>
   );
 };
--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -1,7 +1,29 @@
 import React from "react";
+import { Box, Image, Button } from "@chakra-ui/react";
+import { Link } from "react-router-dom";
 
 const Landing = () => {
-  return <div>Landing</div>;
+  return (
+    <Box
+      bgImage="url('http://www.script-tutorials.com/demos/360/images/stars.png')"
+      bgSize="cover"
+      bgRepeat="no-repeat"
+      h="100vh"
+      display="flex"
+      flexDirection="column"
+      alignItems="center"
+    >
+      <Image
+        src="/images/logo.15c5ace1.png"
+        w="500px"
+        h="500px"
+        objectFit="contain"
+      />
+      <Button colorScheme="teal" bg="#00b1c9" color="#80fc63" size="lg">
+        <Link to="/home">Start</Link>
+      </Button>
+    </Box>
+  );
 };
 
 export default Landing;
